a28a72ada1daa62c16fd8c1271c1cd93014e2932
Colin Walters <walters@verbum.org>
2014-02-23 15:06:46 -0500

    Fork from fedora-atomic

--- web/app.js
+++ web/app.js
@@ -11,24 +11,8 @@
             when('/', {
                 templateUrl: 'partials/home.html'
 	    }).
-            when('/background', {
-                templateUrl: 'partials/background.html'
-            }).
-            when('/build-status', {
-                templateUrl: 'partials/build-status.html',
-                controller: 'fatomicHomeCtrl'
-            }).
-            when('/development', {
-                templateUrl: 'partials/development.html'
-            }).
-            when('/implementation', {
-                templateUrl: 'partials/implementation.html'
-            }).
-            when('/installation', {
-                templateUrl: 'partials/installation.html'
-            }).
-            when('/running-custom', {
-                templateUrl: 'partials/running-custom.html'
+            when('/download', {
+                templateUrl: 'partials/download.html'
             }).
             otherwise({
                 redirectTo: 'partials/unknown.html',
